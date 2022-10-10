46318c5d42796de3c3a60cb2236552eab1a77ec3
Suert <vaniaurdenko@gmail.com>
2022-10-10 03:25:08 +0200

    changed some styles for mobile + deleted console.log

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,7 @@
 
 const Card = ({ course_id, name, org, start_display, image, short_description, blocks_url}) => {
-    console.log(course_id)
     return(
-    <div class="information card" className="h-full container mx-auto p-6 hover:shadow-2xl max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className="h-full container mx-auto p-6 hover:shadow-2xl max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
         <img className="object-cover h-48 w-96" src={image} alt={name} />
         <div className="p-5">
             <h5 data-testid="name" className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ const Header = () => {
                 </div>
 
                 <div className="md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0">
-                    <a href="/" className="p-2 lg:px-4 md:mx-2 text-white rounded hover:font-medium">Home</a>
-                    <a href="/" className="p-2 lg:px-4 md:mx-2 text-slate-200 rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">Courses</a>
-                    <a href="/" className="p-2 lg:px-4 md:mx-2 text-slate-200 rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">Programms & Degrees</a>
-                    <a href="/" className="p-2 lg:px-4 md:mx-2 text-slate-200 rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">Schools & Partners</a>
-                    <a href="/" className="p-2 lg:px-4 md:mx-2 text-slate-200 rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">Contact</a>
-                    <a href="/" data-testid="login" className="p-2 lg:px-4 md:mx-2 text-white text-center border border-transparent rounded hover:bg-indigo-100 hover:text-indigo-700 transition-colors duration-300">Login</a>
-                    <a href="/" data-testid="signup" className="p-2 lg:px-4 md:mx-2 text-white text-center border border-solid border-blue-600 rounded hover:border-blue-200  transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Signup</a>
+                    <a href="/" className="p-1 lg:px-4 md:mx-2 text-white rounded hover:font-medium">Home</a>
+                    <a href="/" className="p-1 lg:px-4 md:mx-2 text-slate-200 rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">Courses</a>
+                    <a href="/" className="p-1 lg:px-4 md:mx-2 text-slate-200 rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">Programms & Degrees</a>
+                    <a href="/" className="p-1 lg:px-4 md:mx-2 text-slate-200 rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">Schools & Partners</a>
+                    <a href="/" className="p-1 lg:px-4 md:mx-2 text-slate-200 rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">Contact</a>
+                    <a href="/" data-testid="login" className="p-1 lg:px-4 md:mx-4 text-white text-center border border-transparent rounded hover:bg-indigo-100 hover:text-indigo-700 transition-colors duration-300">Login</a>
+                    <a href="/" data-testid="signup" className="p-1 lg:px-4 md:mx-4 text-white text-center border border-solid border-blue-600 rounded hover:border-blue-200  transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Signup</a>
                 </div>
             </div>
         </nav>
